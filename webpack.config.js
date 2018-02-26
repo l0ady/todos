baf03b1e48ecfef357ffcf13b665550da0f526d1
Guohui Cao <546451014@qq.com>
2018-02-26 18:13:05 +0800

    update css webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const webpack = require('webpack')
+const ExtractPlugin = require('extract-text-webpack-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -20,27 +21,6 @@ config = {
 			{
 				test: /\.jsx$/,
 				loader: 'babel-loader'
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.styl$/,
-				use: [
-					'style-loader',
-					'css-loader',
-					{
-						loader: 'postcss-loader',
-						options: {
-							sourceMap: true
-						}
-					},
-					'stylus-loader'
-				]
 			},
 			{
 				test: /\.(gif|jpg|jpeg|png|svg)$/,
@@ -68,6 +48,20 @@ config = {
 
 if(isDev){//开发环境
 	config.devtool = '#cheap-module-eval-source-map'
+	config.module.rules.push({
+				test: /\.styl$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					{
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true
+						}
+					},
+					'stylus-loader'
+				]
+			})
 	config.devServer = {
 		port: 8000,
 		host: '0.0.0.0',
@@ -80,6 +74,27 @@ if(isDev){//开发环境
 	config.plugins.push(
 		new webpack.HotModuleReplacementPlugin(),
 		new webpack.NoEmitOnErrorsPlugin()
+		)
+}else{
+	config.output.filename = '[name].[chunkhash:8].js'
+	config.module.rules.push({
+				test: /\.styl$/,
+				use: ExtractPlugin.extract({
+					fallback: 'style-loader',
+					use:[
+						'css-loader',
+						{
+							loader: 'postcss-loader',
+							options: {
+								sourceMap: true
+							}
+						},
+						'stylus-loader'
+					]
+				})
+			})
+	config.plugins.push(
+			new ExtractPlugin('styles.[contentHash:8].css')
 		)
 }
 
